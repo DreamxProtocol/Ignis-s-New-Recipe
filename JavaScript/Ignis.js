fbf97e856e762c72ef385fd624a21e0c3473c0db
Marcelino <93845376+DreamxProtocol@users.noreply.github.com>
2021-12-20 23:24:46 -0800

    added a "hamburger" nav and slideshow is in the works

---
+++ JavaScript/Ignis.js
@@ -0,0 +1,8 @@
+function toggle() {
+    const x = document.getElementById("navv");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+}
