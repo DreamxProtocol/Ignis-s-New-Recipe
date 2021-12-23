63a15ad24eeabdca1910f305e616425e70c1b630
Marcelino <93845376+DreamxProtocol@users.noreply.github.com>
2021-12-22 23:57:54 -0800

    fixed some layout with css and added a lil JavaScript... unfortunately, no slideshow

--- JavaScript/Ignis.js
+++ JavaScript/Ignis.js
@@ -6,3 +6,42 @@ function toggle() {
         x.style.display = "none";
     }
 }
+
+
+
+function firstItem() {
+    const first = document.getElementById("featured_items0");
+    const second = document.getElementById("featured_items1");
+    const third = document.getElementById("featured_items2");
+
+    first.style.display = "block";
+    second.style.display = "none";
+    third.style.display = "none";
+}
+
+function secondItem() {
+    const first = document.getElementById("featured_items0");
+    const second = document.getElementById("featured_items1");
+    const third = document.getElementById("featured_items2");
+
+    first.style.display = "none";
+    second.style.display = "block";
+    third.style.display = "none";
+}
+
+function thirdItem() {
+    const first = document.getElementById("featured_items0");
+    const second = document.getElementById("featured_items1");
+    const third = document.getElementById("featured_items2");
+
+    first.style.display = "none";
+    second.style.display = "none";
+    third.style.display = "block";
+}
+
+
+
+    
+
+
+
